964fb05938e9eca43d69af610105ca1d4fedfebd
CFelt22 <charles.felteau@gmail.com>
2023-04-01 05:14:53 +0000

    reverting changes

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -26,20 +26,22 @@ class Db:
   def init_pool(self):
     connection_url = os.getenv("CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
-  # we want to commit data such as an insert
-  # be sure to check for RETURNING in all uppercases
+
   def print_params(self,params):
     blue = '\033[94m'
     no_color = '\033[0m'
-    print(f'{blue} SQL Params:{no_color}')
+    print(f"{blue} SQL PARAMS {no_color}")
     for key, value in params.items():
       print(key, ":", value)
 
   def print_sql(self,title,sql):
     cyan = '\033[96m'
     no_color = '\033[0m'
-    print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
+    print(f"{cyan} SQL STATEMENT [{title} ------{no_color}")
     print(sql)
+    
+  # When we want to commit data such as an insert
+  # Be sure to check for RETURNING in all uppercases
   def query_commit(self,sql,params={}):
     self.print_sql('commit with returning',sql)
 
@@ -57,7 +59,8 @@ class Db:
           return returning_id
     except Exception as err:
       self.print_sql_err(err)
-  # when we want to return a json object
+
+  # When we want to return an array of json objects
   def query_array_json(self,sql,params={}):
     self.print_sql('array',sql)
 
@@ -67,7 +70,8 @@ class Db:
         cur.execute(wrapped_sql,params)
         json = cur.fetchone()
         return json[0]
-  # When we want to return an array of json objects
+
+  # When we want to return a json object
   def query_object_json(self,sql,params={}):
 
     self.print_sql('json',sql)
@@ -82,25 +86,30 @@ class Db:
           "{}"
         else:
           return json[0]
-  def query_wrap_object(self,template):
+  def query_wrap_object(self, template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
     {template}
     ) object_row);
     """
     return sql
-  def query_wrap_array(self,template):
+
+  def query_wrap_array(self, template):
     sql = f"""
     (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
     {template}
     ) array_row);
     """
     return sql
+
+  # define a function that handles and parses psycopg2 exceptions
   def print_sql_err(self,err):
     # get details about the exception
     err_type, err_obj, traceback = sys.exc_info()
+
     # get the line number when exception occured
     line_num = traceback.tb_lineno
+
     # print the connect() error
     print ("\npsycopg ERROR:", err, "on line number:", line_num)
     print ("psycopg traceback:", traceback, "-- type:", err_type)
@@ -108,4 +117,5 @@ class Db:
     # print the pgcode and pgerror exceptions
     print ("pgerror:", err.pgerror)
     print ("pgcode:", err.pgcode, "\n")
+
 db = Db()
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,10 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+
 from lib.db import db
+
 #tracer = trace.get_tracer("home.activities")
+
 class HomeActivities:
   def run(cognito_user_id=None):
     #logger.info("HomeActivities")
@@ -11,3 +14,4 @@ class HomeActivities:
     #  span.set_attribute("app.now", now.isoformat())
     sql = db.template('activities','home')
     results = db.query_array_json(sql)
+    return results
