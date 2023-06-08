8311322278709a14d1451b41efdf5724fd9dcdf7
CFelt22 <charles.felteau@gmail.com>
2023-06-08 10:22:57 -0400

    finnish up initialization refactor

--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -2,7 +2,10 @@ import './ProfileAvatar.css';
 
 export default function ProfileAvatar(props) {
   const defaultImage = 'https://assets.cruddur.com/avatars/undefined.jpg';
-  const backgroundImage = `url("https://assets.cruddur.cfelteau.ca/avatars/${props.id}.jpg"), url(${defaultImage})`;
+  const backgroundImage = props.id
+    ? `url("https://assets.cruddur.cfelteau.ca/avatars/${props.id}.jpg")`
+    : `url(${defaultImage})`;
+  
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
