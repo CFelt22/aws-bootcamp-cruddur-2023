2f95aa028cd0ec2223d3ebe8d5716c3eaa0570bf
CFelt22 <charles.felteau@gmail.com>
2023-06-07 22:55:18 -0400

    refactor jwt to use decorator, clic to clos window

--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -1,7 +1,8 @@
 import './ProfileAvatar.css';
 
 export default function ProfileAvatar(props) {
-  const backgroundImage = `url("https://assets.cruddur.cfelteau.ca/avatars/${props.id}.jpg")`;
+  const defaultImage = 'https://assets.cruddur.com/avatars/undefined.jpg';
+  const backgroundImage = `url("https://assets.cruddur.cfelteau.ca/avatars/${props.id}.jpg"), url(${defaultImage})`;
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
