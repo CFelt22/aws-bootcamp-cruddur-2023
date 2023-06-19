3b5bc8f5baba945db77b461877e10a339b22be8a
CFelt22 <charles.felteau@gmail.com>
2023-06-19 21:12:13 +0000

    trying to add a default image in the app

--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -1,21 +1,33 @@
 import './ProfileAvatar.css';
+import React from 'react';
 
 export default function ProfileAvatar(props) {
-  const defaultImage = 'https://assets.cruddur.com/avatars/undefined.jpg';
-  const backgroundImage = props.id
-    ? `url("https://assets.cruddur.cfelteau.ca/avatars/${props.id}.jpg")`
-    : `url(${defaultImage})`;
-  
+  const [imageSrc, setImageSrc] = `https://assets.cruddur.cfelteau.ca/avatars/${props.id}.jpg`;
+  const fallbackSrc = `https://assets.cruddur.cfelteau.ca/avatars/undefined.jpg`;
+  const backgroundImage = `url("https://assets.cruddur.cfelteau.ca/avatars/${props.id}.jpg")`
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   };
 
+  React.useEffect(()=>{
+    const checkImage = async () => {
+      try {
+        const response = await fetch(imageSrc);
+        if (response.status === 403) {
+          setImageSrc(fallbackSrc);
+        }
+      } catch (error) {
+        console.error('Error checking image:', error);
+        setImageSrc(fallbackSrc);
+      }
+    };
+
+    checkImage();
+  }, [imageSrc]);
+
   return (
-    <div 
-      className="profile-avatar"
-      style={styles}
-    ></div>
+    <div className="profile-avatar" style={styles}></div>
   );
 }
